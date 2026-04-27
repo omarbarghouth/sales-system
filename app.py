e814bb0cde068adee962cbdd9b48ee968f5617da
omarbarghouth <58629211+omarbarghouth@users.noreply.github.com>
2026-04-27 16:13:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -136,3 +136,5 @@ if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
